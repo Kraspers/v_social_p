6463c7ea478ad0b6e24528da1697a002651b1f9f
Kraspers <bebraslaf@gmail.com>
2026-04-02 12:19:19 +0300

    Remove env dependency from app bootstrap to fix Render reference error

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,10 @@
 const expressLib = require('express');
-const routes = require('./routes');
 const errorMiddleware = require('./middleware/error.middleware');
 const notFoundMiddleware = require('./middleware/not-found.middleware');
 
 const app = expressLib();
 app.use(require('helmet')());
-app.use(require('cors')({ origin: env.corsOrigin, credentials: true }));
+app.use(require('cors')({ origin: process.env.CORS_ORIGIN || '*', credentials: true }));
 app.use(expressLib.json({ limit: '2mb' }));
 app.use(require('cookie-parser')());
 app.use(require('morgan')('dev'));
